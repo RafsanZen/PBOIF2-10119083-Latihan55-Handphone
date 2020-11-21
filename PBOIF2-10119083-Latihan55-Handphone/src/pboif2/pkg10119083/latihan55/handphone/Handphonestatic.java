3fc6a65063f6685339646386aa0d91b4f1bbe74c
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-11-22 01:23:31 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan55-Handphone/src/pboif2/pkg10119083/latihan55/handphone/Handphonestatic.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan55.handphone;
+
+/**
+ *
+ * @author hendrawan
+ */
+class Handphonestatic {
+    
+}
